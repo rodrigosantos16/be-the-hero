06a2aefd23646768a758d498a4bc586be250ae15
rodrigosantos16 <rod.santos014@gmail.com>
2020-03-28 01:52:08 -0300

    Back-End da aplicação

---
+++ backend/knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+const path = require('path');
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, './src/database/db.sqlite'),
+    },
+    migrations: {
+      directory: path.join(__dirname, './src/database/migrations'),
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
